dcf940e44d090a22e8a8878f994f1c6eb7958fe3
studboii <studboii@users.noreply.github.com>
2020-11-09 12:20:34 +0530

    Add a basic index to check, changed some part of License and added a gitignore to modify as we'd like later on
    Added: gitignore, ignores Dependecies at the moment. They'll be added on later.
    Basic index.php to test and start off with. and changes to copyright in License

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is the index</title>
+</head>
+<body>
+	<?php 
+	echo "Hey, if this loads in, everything is set-up and fine.";
+	?>
+
+</body>
+</html>
