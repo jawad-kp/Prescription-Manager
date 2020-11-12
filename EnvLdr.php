8e46c3c27cb8defd19a9f3c5e6bdef49317a53d3
studboii <studboii@users.noreply.github.com>
2020-11-12 13:59:36 +0530

    EnvLdr and DBConfig are set-up now. One loads in our Environment variables and the other sets-up our database with it

---
+++ EnvLdr.php
@@ -0,0 +1,8 @@
+<?php
+
+	require_once __DIR__.'\\vendor\\autoload.php';
+	use \Dotenv\Dotenv;
+	$dotenv = Dotenv::createImmutable(__DIR__);
+	$dotenv->load(); 
+?>
+<!-- This file load in our environment variables for us. This has to be done just one across pages so it's a single line import now. -->
