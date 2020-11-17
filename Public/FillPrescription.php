e3fd5e529fd31aacbc0187e18e919b788efbe26a
studboii <studboii@users.noreply.github.com>
2020-11-17 13:52:45 +0530

    Incomplete File on hand

---
+++ Public/FillPrescription.php
@@ -0,0 +1,14 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Enter Prescription</title>
+</head>
+<body>
+	<!-- Set-up initial fill and then send requests were you add stuff to DB -->
+	<!-- The response will have more form fields, a submit button to end this task and make apporpriate redirects. -->
+
+</body>
+</html>
