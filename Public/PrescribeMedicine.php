46af2e8a320a91bbf8a48ca4af372eda1d38a912
studboii <studboii@users.noreply.github.com>
2020-11-17 08:16:32 +0530

    A doctor can now look up a patient and find details for them. I still need to do the actual prescription part but this works for now.

---
+++ Public/PrescribeMedicine.php
@@ -0,0 +1,61 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prescribe Medicine</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width= device-width, initial-scale=1">
+  	<style type="text/css">
+  		.error{ color: red; font-size: 16px }
+  	</style>
+  	<link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+  	<link rel="stylesheet" type="text/css" href="../Dependencies/Hover-master/css/hover.css">
+  	<link rel="stylesheet" type="text/css" href="../Dependencies/CSS Animations/animate.css">
+  	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.js"></script>
+  	<script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.js">
+	</script> 
+
+	<script>
+		$(document).ready(function()
+		{
+			$('#Srch').click(function()
+			{
+				var val = $('#PatID').val();//Getting value of  text field.
+				// console.log(val);
+				$.ajax(
+				{
+					type: "POST",
+					url:"ShowPatientDetails.php",
+					data:{PatID: val},
+					success:function (html)
+					{
+						$('#DetailDisp').html(html);
+					}
+				})
+
+			})
+		})
+	</script>
+</head>
+<body>
+
+<?php
+	require "DBConfig.php";
+  	require "InpModder.php";
+?>
+<div id="BigDisp">
+		<label for="PatID">
+			Patient ID 
+		</label>
+			<input type="text" name="PatID" placeholder="Enter Patient ID" class="form-group" id="PatID">
+		<br>
+        <center><button class="btn btn-primary hvr-grow" id="Srch">Search</button></center>
+        <br>
+
+        <div id="DetailDisp"></div>
+</div>
+
+</body>
+</html>
---
+++ Public/ShowPatientDetails.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prescribe Medicine</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width= device-width, initial-scale=1">
+  	<style type="text/css">
+  		.error{ color: red; font-size: 16px }
+  	</style>
+  	<link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+  	<link rel="stylesheet" type="text/css" href="../Dependencies/Hover-master/css/hover.css">
+  	<link rel="stylesheet" type="text/css" href="../Dependencies/CSS Animations/animate.css">
+  	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.js"></script>
+  	<script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.js">
+	</script> 
+</head>
+<body>
+
+<?php 
+	require "DBConfig.php";
+	require __DIR__."/../EncrypterCustom.php";
+	if (!(empty($_POST["PatID"])))
+	{
+		
+		$ID = $_POST["PatID"];
+		$Qry = "SELECT * FROM `patlog` WHERE `PatID` LIKE \"$ID\"";
+		$res = $conn->query($Qry);
+		if($res->num_rows == 1)
+		{
+			$row = $res->fetch_assoc();
+			$name = Decrptr($row["PatName"]);
+			echo "Patient Details are as follows: ";
+			echo "Name: $name<br>";
+			$adr = Decrptr($row["Addr"]);
+			$Dte = $row["PatDOB"];
+			$dob = date("d/m/Y", strtotime($Dte));
+			$gender = $row["PatGen"];
+			echo "Address: $adr<br>DOB(dd/mm/yyyy): $dob<br>Gender: $gender<br><br>";
+			echo "<button id=\"CnfrmID\" onclick=\"window.location.href='EnterPresc.php'\">Confirm</button>";
+			$_SESSION["DocPatID"] = $ID;
+
+		}
+		else
+		{
+			echo "Patient $ID was not found.";
+		}
+	}
+?>
