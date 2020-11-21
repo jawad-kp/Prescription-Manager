d571ada194a1e66a5bdc12dcd77e3932314e6b18
studboii <studboii@users.noreply.github.com>
2020-11-21 19:44:07 +0530

    PrescPage.php is finished on the back.
    This page now shows you a list of medication you've been prescribed and the doctor's diagnosis. There's an option to print this page and go back and forth between the rest.

--- Public/PrescPage.php
+++ Public/PrescPage.php
@@ -10,9 +10,117 @@ if ((!(isset($_SESSION["PatName"]))) || (!(isset($_SESSION["PrescID"]))) )
  <html>
  <head>
  	<title>View Medication</title>
+ 	<meta charset="utf-8">
+  	<meta name="viewport" content="width= device-width, initial-scale=1">
+  	<link rel="stylesheet" type="text/css" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+  	<!-- <link rel="stylesheet" type="text/css" href="../Dependencies/Hover-master/css/hover.css"> -->
+  	<!-- <link rel="stylesheet" type="text/css" href="../Dependencies/CSS Animations/animate.css"> -->
+  	<script type="text/javascript" src="../node_modules/jquery/dist/jquery.js"></script>
+  	<script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.js">
+	</script> 
  </head>
  <body>
- 	Here we be viewing our medication
+ 	<?php 
+ 		require "DBConfig.php";
+
+ 		function TabPrinter($tb,$Fre,$Dsge)
+ 		{
+ 			echo "
+ 			<div class=\"row\">
+		 	 	<div class=\"col\">$tb</div>
+		 	 	<div class=\"col\">$Fre</div>
+		 	 	<div class=\"col\">$Dsge</div>
+ 	 		</div>";
+ 			
+ 		}//This function prints our tablets/medication prescribed.
+ 		
+
+ 		function FetchMedic() //This function fetches existing medication if prescribed and displays it.
+ 		{
+ 			$Qry = "SELECT * FROM `presclog` WHERE `PrescID` LIKE \"".$_SESSION["PrescID"]."\"";
+
+	 		$res = $GLOBALS["conn"]->query($Qry);
+
+	 		if ($res->num_rows > 0)
+	 		{
+
+	 			echo "
+	 			<div class=\"row\">
+			 	 	<div class=\"col\">Tablet Name</div>
+			 	 	<div class=\"col\">Frequecy</div>
+			 	 	<div class=\"col\">Dosage</div>
+	 	 		</div>";
+
+	 			while($row = $res->fetch_assoc())
+	 			{
+	 				TabPrinter($row["MedName"],$row["Frequency"],$row["Dosage"]);
+
+	 			}
+	 		}
+	 		else
+	 		{
+	 			echo "
+	 			<div class=\"row\">
+		 			<div class=\"col\"></div>
+		 			<div class=\"col\">No Specific Medication Prscribed</div>
+		 			<div class=\"col\"></div>
+	 			 </div>";
+	 		}
+ 		}
+
+ 		$RepQuery = "SELECT * FROM `PatList` WHERE `PrescID` LIKE \"".$_SESSION['PrescID']."\"";
+ 		$RepRes = $conn->query($RepQuery);
+ 		if ($RepRes-> num_rows == 1)
+ 		{
+ 			$row = $RepRes->fetch_assoc();
+ 			echo "
+	 			<div class=\"row\">
+			 	 	<div class=\"col\">Patient Name: ".$_SESSION["PatName"]."</div>
+			 	</div>
+
+			 	<div class=\"row\"> 
+			 	 	<div class=\"col\">Report: </div>
+			 	 	<div class=\"col\">".$row["DocReport"]."</div>
+	 	 		</div>
+
+	 	 		<div class=\"row\"> 
+			 	 	<div class=\"col\">Comments: </div>
+			 	 	<div class=\"col\">";
+			 	 if (empty($row["Comments"]))
+			 	 {
+			 	 	echo "No Comments";
+			 	 }
+			 	 else
+			 	 {
+			 	 	echo $row["Comments"];
+			 	 }
+			 	 
+			 	 echo "</div>
+	 	 		</div>";
+
+	 	 	FetchMedic();
+ 		}
+ 		else
+ 		{
+ 			die("Something has gone horribly wrong");
+
+ 		}
+
+ 	 ?>
+
+
+
+ 	 
+
+ 	 <div class="row">
+ 	 	<div class="col-sm-1"></div>
+		<div class="col"><a href="PrescList.php">Back</a></div>
+		<div class="col"><button class="btn btn-info" onclick="window.print()">Print Page</button></div>
+		<div class="col-sm-1"></div>
+	 </div>
+
+
+
  
  </body>
  </html>
